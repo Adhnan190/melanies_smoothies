d4c2a32852e3f5eff6c97a4c70f760051270b3ba
Adhnan190 <adhanan.kuthpudeen@relanto.ai>
2025-05-27 10:38:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,12 @@ ingredients_list=st.multiselect(
     my_dataframe,
     max_selections=5
 )
- 
 if ingredients_list:
     ingredients_string = ''
     for x in ingredients_list:
         ingredients_string +=x + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-    # st.write(ingredients_string)
         my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order + """')"""
         time_to_insert = st.button("Submit Order")
